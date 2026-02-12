9728d4028ed4104d9a18c77d98dda9417bb0a3a4
Tae-gwan <taegwwan@gmail.com>
2026-02-12 15:35:51 +0900

    chore: Docker 최적화 (Dockerfile 추가 및 분리)

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -19,6 +19,13 @@ export const metadata: Metadata = {
   description: "Vibing together on screen, anywhere",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
+  userScalable: true,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
